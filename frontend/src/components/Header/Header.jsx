78f4ca8f22a7ddb54100eee09c79d5d0011c5106
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2024-02-01 16:00:55 +0530

    Home page done

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useRef } from "react";
 import logo from "../../assets/images/logo.png";
 import { NavLink, Link } from "react-router-dom";
-import userImg from '../../assets/images/avatar-icon.png';
+import userImg from "../../assets/images/avatar-icon.png";
+import {BiMenu} from 'react-icons/bi';
+
 const navLinks = [
   {
     path: "/home",
@@ -22,8 +24,30 @@ const navLinks = [
 ];
 
 const Header = () => {
+
+  const headerRef = useRef(null)
+  const menuRef = useRef(null)
+
+  const handleStickyHeader = () => {
+    window.addEventListener('scroll', ()=>{
+      if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
+        headerRef.current.classList.add('sticky__header')
+      }else{
+        headerRef.current.classList.remove('sticky__header')
+      }
+    })
+  }
+
+  useEffect(() =>{
+    handleStickyHeader()
+
+    return ()=> window.removeEventListener('scroll', handleStickyHeader)
+  })
+
+  const toggleMenu = () => menuRef.current.classList.toggle('show__menu')
+
   return (
-    <header className="header flex items-center">
+    <header className="header flex items-center" ref={headerRef}>
       <div className="container">
         <div className="flex items-center justify-between">
           {/* =============logo =============== */}
@@ -32,7 +56,7 @@ const Header = () => {
           </div>
 
           {/* =========menu ============= */}
-          <div className="navigation">
+          <div className="navigation" ref={menuRef} onClick={toggleMenu}>
             <ul className="menu flex items-center gap-[2.7rem]">
               {navLinks.map((link, index) => (
                 <li key={index}>
@@ -53,18 +77,23 @@ const Header = () => {
 
           {/* ============ nav right ========== */}
           <div className="flex items-center gap-4">
-            <div>
-              <Link to='/'>
+            <div className="hidden">
+              <Link to="/">
                 <figure className="w-[35px] rounded-full cursor-pointer">
                   <img src={userImg} className="w-full rounded-full" alt="" />
                 </figure>
               </Link>
             </div>
 
-            <Link to='/login'>
-            <button className="bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center justify-center rounded-[50px]">Login</button>
+            <Link to="/login">
+              <button className="bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center justify-center rounded-[50px]">
+                Login
+              </button>
             </Link>
 
+            <span className="md: hidden" onClick={toggleMenu}>
+              <BiMenu className="w-6 h-6 cursor-pointer" />
+            </span>
           </div>
         </div>
       </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,80 @@
 import React from "react";
+import heroImg01 from "../assets/images/hero-img01.png";
+import heroImg02 from "../assets/images/hero-img02.png";
+import heroImg03 from "../assets/images/hero-img03.png";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      {/* ======= hero section ========== */}
+      <>
+        <section className="hero__section pt-[60px] 2xl:h-[800px]">
+          <div className="container">
+            <div className="flex flex-col lg:flex-row gap-[90px] items-center justify-between">
+              {/* ============hero content ============ */}
+              <div>
+                <div className="lg:w-[570px]">
+                  <h1 className="text-[36px] leading-[46px] text-headingColor font-[800] md:text-[60px] md:leading-[70px]">
+                    We help patients live a healthy, longer life.
+                  </h1>
+                  <p className="text__para">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Ipsam suscipit eveniet vero hic voluptate ex soluta!
+                    Voluptas veritatis nulla quos hic et inventore laborum amet,
+                    eius, dolorum vel optio neque. Iure, accusamus accusantium
+                    tempora ipsam, omnis dignissimos saepe magnam vero, dolor
+                    animi in asperiores rem sequi consequatur pariatur repellat
+                    nobis?
+                  </p>
+
+                  <button className="btn">Request an Appointment</button>
+                </div>
+
+                {/* ================HERO Counter ===================== */}
+
+                <div className="mt-[30px] lg:mt-[70px] flex flex-col lg:flex-row lg:items-center gap-5 lg:gap-[30px]">
+                  <div>
+                    <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
+                      30+
+                    </h2>
+                    <span className="w-[100px] h-2 bg-yellowColor reonded-full block mt-[-14px]"></span>
+                    <p className="text__para">Years of Experience</p>
+                  </div>
+
+                  <div>
+                    <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
+                      15+
+                    </h2>
+                    <span className="w-[100px] h-2 bg-purpleColor reonded-full block mt-[-14px]"></span>
+                    <p className="text__para">Clinic Location</p>
+                  </div>
+                  <div>
+                    <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
+                      100%
+                    </h2>
+                    <span className="w-[100px] h-2 bg-irisBlueColor reonded-full block mt-[-14px]"></span>
+                    <p className="text__para">Patient Satisfaction</p>
+                  </div>
+                </div>
+              </div>
+
+              {/*==============hero content============== */}
+
+              <div className="flex gap-[30px] justify-end">
+                <div>
+                  <img className="w-full" src={heroImg01} alt="" />
+                </div>
+                <diV className="mt-[30px]">
+                  <img src={heroImg02} alt="" className="w-full mb-[30px]" />
+                  <img src={heroImg03} alt="" className="w-full" />
+                </diV>
+              </div>
+            </div>
+          </div>
+        </section>
+      </>
+    </>
+  );
 };
 
 export default Home;
