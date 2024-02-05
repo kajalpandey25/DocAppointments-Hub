3c0dff21bb50e13237f1f8c6cfda0aed25f31af7
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2024-02-05 15:59:20 +0530

    Home page done

--- frontend/src/pages/Doctors/DoctorDetails.jsx
+++ frontend/src/pages/Doctors/DoctorDetails.jsx
@@ -1,7 +1,61 @@
 import React from "react";
+import doctorImg from "../../assets/images/doctor-img02.png";
+import starIcon from "../../assets/images/Star.png";
 
 const DoctorDetails = () => {
-  return <div>DoctorDetails</div>;
+
+  return (
+    <section>
+      <div className="max-w-[1170px] px-5 mx-auto">
+        <div className="grid md:grid-cols-3 gap-[50px]">
+          <div className="md:col-span-2">
+            <div className="flex items-center gap-5">
+              <figure className="max-w-[200px] max-h-[200px]">
+                <img src={doctorImg} alt="" className="w-full" />
+              </figure>
+
+              <div>
+                <span className="bg-[#CCF0F3] text-irisBlueColor py-1 px-6 lg:py-2 lg:px-6 text-[12px] leading-4 lg:text-[16px] lg:leading-7 font-semibold rounded">
+                  Surgeon
+                </span>
+                <h3 className="text-headingColor text-[22px] leading-9 mt-3 font-bold">
+                  Muhibur Rahman
+                </h3>
+                <div className="flex items-center gap-[6px]">
+                  <span className="flex items-center gap-[6px] text-[14px] leading-5 lg:text-[16px] lg:leading-7 font-semibold text-headingColor">
+                    <img src={starIcon} alt="" /> 4.8
+                  </span>
+                  <span className="text-[14px] leading-5 lg:text-[16px] lg:leading-7 font-[400] text-textColor">
+                    (272)
+                  </span>
+                </div>
+
+                <p className="text__para text-[14px] leading-6 md:text-[15px] lg:max-w-[390px]">
+                  "Expert surgeon: Skillful hands, precision, and compassion,
+                  transforming lives through surgical mastery."
+                </p>
+              </div>
+            </div>
+
+            <div className="mt-[50px] border-b border-solid border-[#0066ff34]">
+              <button
+                className={`py-2 px-5 mr-5 text-[16px] leading-7 text-headingColor font-semibold`}
+              >
+                About
+              </button>
+              <button
+                className={`py-2 px-5 mr-5 text-[16px] leading-7 text-headingColor font-semibold`}
+              >
+                Feedback
+              </button>
+            </div>
+          </div>
+
+          <div></div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default DoctorDetails;
