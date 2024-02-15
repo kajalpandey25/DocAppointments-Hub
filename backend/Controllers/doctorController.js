e945e0597ed87df798a4cda51c01a8d0a7f8a1ce
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2024-02-15 15:51:24 +0530

    Backend start

--- backend/Controllers/doctorController.js
+++ backend/Controllers/doctorController.js
@@ -38,7 +38,9 @@ export const getSingleDoctor = async (req, res) => {
   const id = req.params.id;
 
   try {
-    const doctor = await Doctor.findById(id).select("-password");
+    const doctor = await Doctor.findById(id)
+      .populate("reviews")
+      .select("-password");
 
     res.status(200).json({
       success: true,
@@ -64,7 +66,7 @@ export const getAllDoctor = async (req, res) => {
         ],
       }).select("-password");
     } else {
-      doctors = await Doctor.find({isApproved: "approved" }).select(
+      doctors = await Doctor.find({ isApproved: "approved" }).select(
         "-password"
       );
     }
